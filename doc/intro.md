1f2d7d96ffb63e93f173640bb9ae3985c103aafe
neiro <neiro.mail@gmail.com>
2018-12-22 20:44:07 +0300

    Init the new clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to attendance-api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/attendance_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns attendance-api.core-test
+  (:require [clojure.test :refer :all]
+            [attendance-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
