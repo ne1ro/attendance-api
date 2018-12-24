22f32f637ffda3b9339af1bb63fba893f7088947
neiro <neiro.mail@gmail.com>
2018-12-24 16:36:01 +0300

    Protype actions for attendants

--- src/attendance/application.clj
+++ src/attendance/application.clj
@@ -1,3 +1,12 @@
 (ns attendance.application)
 
+(defn create-attendant [attendant-form] attendant-form)
+
 (defn list-attendants [] [{ "first-name" "Vasya" "last-name" "Pupkin" "id" 1}])
+
+(defn get-attendant [id] { "id" 1 })
+
+(defn delete-attendant [attendant] (attendant))
+
+; Private functions
+; (defn- )
