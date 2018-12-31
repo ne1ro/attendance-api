ee38be544470b042d3308d8aae24a8408fd21925
neiro <neiro.mail@gmail.com>
2018-12-31 21:00:14 +0300

    Test domain/save-attendant func

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Attendance API built in Clojure"
   :url "https://github.com/ne1ro/attendance-api"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/java.jdbc "0.7.8"]
                  [org.xerial/sqlite-jdbc "3.25.2"]
                  [org.clojure/tools.namespace "0.2.11"]
@@ -11,18 +11,20 @@
                  [metosin/compojure-api "1.1.11"]
                  [mount "0.1.15"]
                  [org.clojure/clojure "1.10.0"]
+                 [com.github.javafaker/javafaker "0.16"]
                  [prismatic/schema "1.1.9"]]
-  :ring  {:handler attendance.core/app :nrepl {:start? true :port 9998}}
+  :ring {:handler attendance.core/app :nrepl {:start? true :port 9998}}
   :uberjar-name "attendance.jar"
-  :profiles {:dev {:resource-paths ["src" "config/dev"]
-                   :dependencies [[cljfmt "0.5.1"]]
-                   :plugins [[cider/cider-nrepl "0.18.0"] [lein-autoreload "0.1.1"]
-                             [jonase/eastwood "0.3.3"] [lein-ring "0.12.4"] [lein-kibit "0.1.6"]
-                             [lein-cljfmt "0.6.3"] [mvxcvi/whidbey "2.0.0"]]}
-             :test {:dependencies [[eftest "0.5.4"]]
-                    :resource-paths ["src" "config/test"] :plugins [[lein-eftest "0.5.4"]]}}
+  :profiles {:dev  {:resource-paths ["src" "config/dev"]
+                    :dependencies   [[cljfmt "0.5.1"]]
+                    :plugins        [[cider/cider-nrepl "0.18.0"] [lein-kibit "0.1.6"]
+                                     [jonase/eastwood "0.3.3"] [lein-ring "0.12.4"]
+                                     [lein-cljfmt "0.6.3"] [mvxcvi/whidbey "2.0.0"]]}
+             :test {:dependencies   [[eftest "0.5.4"]]
+                    :resource-paths ["src" "config/test"]
+                    :plugins        [[lein-eftest "0.5.4"]]}}
   :middleware [whidbey.plugin/repl-pprint]
   :main attendance.core
   :repl-options {:init-ns attendance.core
-                 :prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mλ:\u001B[m "))
+                 :prompt  (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mλ:\u001B[m "))
                  :welcome ()})
--- test/attendance/domain_test.clj
+++ test/attendance/domain_test.clj
@@ -1,11 +1,20 @@
 (ns attendance.domain-test
   (:require [clojure.test :refer :all]
-            [attendance.domain :refer :all]))
+            [attendance.domain :refer :all])
+  (:import (com.github.javafaker Faker)))
 
-(def valid-attendant {:day "2018-12-12" :status true :attendantId 0})
+(def faker (Faker.))
+(def valid-attendance {:day "2018-12-12" :status true :attendantId 0})
+(def valid-attendant
+  {:firstName (-> faker .name .firstName) :lastName (-> faker .name .lastName)})
 
 (deftest attend-test
-  (is (thrown-with-msg? Exception #"Value does not match schema:" (attend nil)))
   (is (thrown? Exception (attend {})))
   (is (thrown? Exception (attend {:day "17-17-17"})))
-  (is (= "2018-12-12" (-> valid-attendant attend :day))))
+  (is (= "2018-12-12" (-> valid-attendance attend :day))))
+
+(deftest save-attend-test
+  (is (thrown? Exception (save-attendant nil)))
+  (is (thrown? Exception (save-attendant {})))
+  (is (thrown? Exception (save-attendant {:firstName nil :lastName nil})))
+  (is (= (:firstName valid-attendant) (-> valid-attendant save-attendant :firstName))))
