d0af63aba8561655e8d6fa22c50ebfc82c1ea8d5
neiro <neiro.mail@gmail.com>
2018-12-31 20:45:58 +0300

    Test domain/attend func

--- test/attendance/domain_test.clj
+++ test/attendance/domain_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [attendance.domain :refer :all]))
 
-(deftest attend-test (testing "Throw error on nil" ((
-                                                      is (thrown? (Exception (attend (nil)))))
-                                                     )))
+(def valid-attendant {:day "2018-12-12" :status true :attendantId 0})
+
+(deftest attend-test
+  (is (thrown-with-msg? Exception #"Value does not match schema:" (attend nil)))
+  (is (thrown? Exception (attend {})))
+  (is (thrown? Exception (attend {:day "17-17-17"})))
+  (is (= "2018-12-12" (-> valid-attendant attend :day))))
