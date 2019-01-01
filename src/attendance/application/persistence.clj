ca755d04dcde98344ea12f9a067ed2a64bdc87b2
neiro <neiro.mail@gmail.com>
2019-01-01 20:55:51 +0300

    Create a persistence port

---
+++ src/attendance/application/persistence.clj
@@ -0,0 +1,14 @@
+(ns attendance.application.persistence)
+
+(defprotocol Persistence
+  "Abstraction for attendance persistence operations"
+  (list-attendants [this] "Returns list of attendants")
+  (get-attendant [this id] "Returns attendant by ID or nil")
+  (create-attendant [this attendant-form] "Creates attendant")
+  (delete-attendant [this id] "Deletes attendant by ID")
+  (list-attendances-days [this] "Returns list of days where were attendances")
+  (list-attendances-by-attendant [this id] "Filters attendances by attendant ID")
+  (list-attendances [this day] "Returns attendances filtered by day")
+  (get-attendance-by-day [this attendant-id day] "Gets attendance by attendant ID and day")
+  (create-attendance [this form] "Creates attendance mark")
+  (delete-attendance [this attendance] "Removes attendance mark"))
