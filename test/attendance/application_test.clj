035aabc0f5a0a6e83eeffa47aa46ad171b4ef0c5
neiro <neiro.mail@gmail.com>
2019-02-11 17:34:48 +0300

    Use redefs

--- test/attendance/application_test.clj
+++ test/attendance/application_test.clj
@@ -1,10 +1,13 @@
 (ns attendance.application-test
-  (:require [clojure.test :refer :all] [attendance.application :refer :all])
+  (:require [clojure.test :refer :all]
+            [attendance.application :refer :all]
+            [attendance.infrastructure.persistence-test [:refer ->PersistenceTest]])
   (:import (com.github.javafaker Faker)))
 
 (def faker (Faker.))
 
 (deftest token-exists?-test
-  (is (thrown? Exception (token-exists? nil)))
-  (is (thrown? Exception (token-exists? 0)))
-  (is (token-exists? "test")))
+  (with-redefs [->PersistenceSQLite ->PersistenceTest]
+    (is (thrown? Exception (token-exists? nil)))
+    (is (thrown? Exception (token-exists? 0)))
+    (is (token-exists? "test"))))
